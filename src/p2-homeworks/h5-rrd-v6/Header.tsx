1b9b02ff95d7afba4f3a7529a0bafc3656dd0ec4
DmitryKhiliuk <khiliukbrest@gmail.com>
2022-04-02 18:32:54 +0300

    5 task + menu

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -5,13 +5,20 @@ import s from './Header.module.css'
 
 function Header() {
     return (
-        <div className={s.nav}>
-            <NavLink to={PATH.PRE_JUNIOR} className={el => el.isActive ? `${s.active} ${s.item}` : s.item}>Недоджун</NavLink>
-            <NavLink to={PATH.JUNIOR} className={el => el.isActive ? `${s.active} ${s.item}` : s.item}>Джун</NavLink>
-            <NavLink to={PATH.POST_JUNIOR} className={el => el.isActive ? `${s.active} ${s.item}` : s.item}>Сверхджун</NavLink>
-            <NavLink to={'/*'} className={el => el.isActive ? `${s.active} ${s.item}` : s.item}>Сеньор</NavLink>
+        <div className={s.burgerMenu}>
+            <input type="checkbox" id={s.menuToggle}/>
+            <label htmlFor={s.menuToggle} className={s.menuBtn}>
+                <span/>
+            </label>
+            <div className={s.nav}>
+                <NavLink to={PATH.PRE_JUNIOR} className={el => el.isActive ? `${s.active} ${s.item}` : s.item}>Недоджун</NavLink>
+                <NavLink to={PATH.JUNIOR} className={el => el.isActive ? `${s.active} ${s.item}` : s.item}>Джун</NavLink>
+                <NavLink to={PATH.POST_JUNIOR} className={el => el.isActive ? `${s.active} ${s.item}` : s.item}>Сверхджун</NavLink>
+                <NavLink to={'/*'} className={el => el.isActive ? `${s.active} ${s.item}` : s.item}>Сеньор</NavLink>
+            </div>
         </div>
     )
 }
 
 export default Header
+
